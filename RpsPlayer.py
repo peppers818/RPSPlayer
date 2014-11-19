30ee645ffd9b67cd11c904be40949c7fe5d28fa2
peppers818 <littlepanniz818@msn.com>
2014-11-19 15:18:58 -0500

    Added Gambit AI
    Removed the check for first turn. Instead the AI uses 1 of 8
    predetermined 3 move gambits. It will randomly choose 1 gambit per
    match.

--- RpsPlayer.py
+++ RpsPlayer.py
@@ -1,27 +1,86 @@
 #Samantha Holloway 11/2014
 #Rock Paper Scissors implementation of Player class
+#A gambit is a predetermined RPS strategy
+#AI uses 1 of 8 famous gambits randomly for a match
 import random
 
 class RpsPlayer():
     def __init__(self):
-        self.turnOne = True
+        self.move = random.randint(1,8)# choose random gambit
+        self.currentMove = 0
+        
     #Provides AI for making a move, returns move as a string
     #move is either Rock, Paper, or Scissors
     def play(self):
-        if(self.turnOne is False): #Not the first turn
-            move = 1 #algorithm should go here to make move
-                                #Plan to have list of previous moves
-        else: #Turn one, make a random move
-            move = random.randint(1,3)
-            self.turnOne = False #First turn is done
-        if move == 1:
-            return "Rock"
-        elif move == 2:
-            return "Paper"
-        else: #move == 3
-            return "Scissors"
+        if self.move == 1: #Denoument gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Rock"
+            elif self.currentMove == 2:
+                return "Scissors"
+            else:
+                return "Paper"   
+        elif self.move == 2: #Bureaucrat gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Paper"
+            elif self.currentMove == 2:
+                return "Paper"
+            else:
+                return "Paper" 
+        elif self.move == 3: #Crescendo gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Paper"
+            elif self.currentMove == 2:
+                return "Scissors"
+            else:
+                return "Rock" 
+        elif self.move == 4: #Avalanche gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Rock"
+            elif self.currentMove == 2:
+                return "Rock"
+            else:
+                return "Rock" 
+        elif self.move == 5: #Fistfull o' Dollars gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Rock"
+            elif self.currentMove == 2:
+                return "Paper"
+            else:
+                return "Paper" 
+        elif self.move == 6: #Paper Dolls gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Paper"
+            elif self.currentMove == 2:
+                return "Scissors"
+            else:
+                return "Scissors" 
+        elif self.move == 7: #Scissor Sandwich gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Paper"
+            elif self.currentMove == 2:
+                return "Scissors"
+            else:
+                return "Paper" 
+        else: #move == 8, Toolbox gambit
+            self.currentMove += 1
+            if self.currentMove == 1:
+                return "Scissors"
+            elif self.currentMove == 2:
+                return "Scissors"
+            else:
+                return "Scissors" 
+    
+
 
 
 r = RpsPlayer()
+print(r.play())
 print(r.play())
 print(r.play())
