25f2f100535b7b05cbe15e4fff6422c4b05ac443
Sammilynn <hollow71@students.rowan.edu>
2014-11-26 01:12:45 -0500

    Tested and fixed code to work with game-framework

--- SHJPPlayer.py
+++ SHJPPlayer.py
@@ -1,3 +1,4 @@
+__author__ = 'Samantha'
 #Samantha Holloway, Joseph Pannizzo 11/2014
 #Help/code pieces from Dr. Ganesh Baliga
 #Rock Paper Scissors implementation of Player class
@@ -8,18 +9,20 @@ import Player
 import Message
 import random
 
-class SHJPPlayer():
+class SHJPPlayer(Player.Player):
     def __init__(self):
         #Call super class constructor
         Player.Player.__init__(self)
         self.history = ""
         self.reset()
 
+
     def play(self):
-        return SHJPPlayerStrategy.play
+        return SHJPPlayerStrategy.play()
 
     def reset(self):
         self.history = ""
+
 
     def notify(self,message):
         self.history = message  #We don't use history for our strategy
@@ -27,90 +30,93 @@ class SHJPPlayer():
     def get_name(self):
         return "Sam-n-Joe"
 
-class SHJPPlayerStrategy(object):
 
-    def __init__(self):
-        self.move = random.randint(1,8)  # choose random gambit
-        self.currentMove = 0
+
+class SHJPPlayerStrategy(object):
+    move = random.randint(1,8)  # choose random gambit
+    current_move = 0
+
+    #Gambits are only 3 moves a piece,used to reset if we get to 4
+    @staticmethod
+    def reset():
+        SHJPPlayerStrategy.current_move = 0
+        SHJPPlayerStrategy.move = random.randint(1,8)
 
     #Provides AI for making a move, returns move as a string
     #move is either Rock(0), Paper(1), or Scissors(2)
     @staticmethod
-    def play(self):
-        if self.move == 1: #Denoument gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "0"
-            elif self.currentMove == 2:
-                return "2"
-            else:
-                return "1"
-        elif self.move == 2: #Bureaucrat gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "1"
-            elif self.currentMove == 2:
-                return "1"
-            else:
-                return "1"
-        elif self.move == 3: #Crescendo gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "1"
-            elif self.currentMove == 2:
-                return "2"
-            else:
-                return "0"
-        elif self.move == 4: #Avalanche gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "0"
-            elif self.currentMove == 2:
-                return "0"
-            else:
-                return "0"
-        elif self.move == 5: #Fistfull o' Dollars gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "0"
-            elif self.currentMove == 2:
-                return "1"
-            else:
-                return "1"
-        elif self.move == 6: #Paper Dolls gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "1"
-            elif self.currentMove == 2:
-                return "2"
-            else:
-                return "2"
-        elif self.move == 7: #Scissor Sandwich gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "1"
-            elif self.currentMove == 2:
-                return "2"
-            else:
-                return "1"
-        else: #move == 8, Toolbox gambit
-            self.currentMove += 1
-            if self.currentMove == 1:
-                return "2"
-            elif self.currentMove == 2:
-                return "2"
-            else:
-                return "2"
+    def play():
 
-if __name__ == "__main__":
-    player = SHJPPlayer()
-    opponent = SHJPPlayer()
-    players = [opponent,player]
-    fakeinfo = ((0,1),1)
-    fakeresult = 1
-    fakemoves = (1,2)
-    player.notify(Message.Message.get_match_start_message(players))
-    player.notify(Message.Message.get_round_start_message(players))
-    move = player.play()
-    print ("Move played: ", move)
-    player.notify(Message.Message.get_round_end_message(players,fakemoves,fakeresult))
+        if SHJPPlayerStrategy.move == 1: #Denoument gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 0
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 2
+            else:
+                SHJPPlayerStrategy.reset()
+                return 1
+        elif SHJPPlayerStrategy.move == 2: #Bureaucrat gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 1
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 1
+            else:
+                SHJPPlayerStrategy.reset()
+                return 1
+        elif SHJPPlayerStrategy.move == 3: #Crescendo gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 1
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 2
+            else:
+                SHJPPlayerStrategy.reset()
+                return 0
+        elif SHJPPlayerStrategy.move == 4: #Avalanche gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 0
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 0
+            else:
+                SHJPPlayerStrategy.reset()
+                return 0
+        elif SHJPPlayerStrategy.move == 5: #Fistfull o' Dollars gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 0
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 1
+            else:
+                SHJPPlayerStrategy.reset()
+                return 1
+        elif SHJPPlayerStrategy.move == 6: #Paper Dolls gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 1
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 2
+            else:
+                SHJPPlayerStrategy.reset()
+                return 2
+        elif SHJPPlayerStrategy.move == 7: #Scissor Sandwich gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 1
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 2
+            else:
+                SHJPPlayerStrategy.reset()
+                return 1
+        else: #SHJPPlayerStrategy.move == 8, Toolbox gambit
+            SHJPPlayerStrategy.current_move += 1
+            if SHJPPlayerStrategy.current_move == 1:
+                return 2
+            elif SHJPPlayerStrategy.current_move == 2:
+                return 2
+            else:
+                SHJPPlayerStrategy.reset()
+                return 2
+
